c31d12c20be99f58565e8f0803c1d6e3c243995e
valentina978 <114037877+valentina978@users.noreply.github.com>
2023-11-07 22:10:22 -0500

    commit4,video7
    commit del video 7

--- 07-bonus-loginAPP/src/app/pages/registro/registro.component.ts
+++ 07-bonus-loginAPP/src/app/pages/registro/registro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -8,16 +8,24 @@ import { UsuarioModel } from 'src/app/models/usuario.model';
 })
 export class RegistroComponent implements OnInit {
 
-usuario:UsuarioModel= new UsuarioModel();
+  usuario: UsuarioModel = new UsuarioModel();
+
+
   constructor() { }
 
-  ngOnInit() { 
-    this.usuario= new UsuarioModel();
-    this.usuario.email='valecan2404@gmail.com';
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+    this.usuario.email = 'fernando.herrera85@gmail.com';
   }
-  onSubmit(){
+
+  onSubmit( form: NgForm){
+
+    if( form.invalid){return}
+
+
     console.log('Formulario enviado');
-    console.log(this.usuario);
+    console.log(this.usuario)
+    console.log(form)
   }
 
 }
